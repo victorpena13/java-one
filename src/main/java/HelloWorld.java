e7333b740457fbd0ad3de1aa186329ecd4d2ecd0
Victor Pena <13victor.pena@gmail.com>
2023-07-05 07:43:54 -0500

    general practice syntax for java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("code inside curly brackets should be indented");
+        System.out.println("with 4 spaces");
     }
 }
