8bddb015f07f00dc5abbc7c919603a7e52ce88f9
Victor Pena <13victor.pena@gmail.com>
2023-07-05 08:12:04 -0500

    data types

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -10,10 +10,24 @@ public class HelloWorld {
         /* I take up one line */
 
 /**
- * I take up
- * multiple
- * lines
+ * 2 main categories of types in java: primitive and reference.
+ * primitive - are 'building blocks' of the language.
+ * reference - types are those that refer to objects or arrays; default to special  value null - indicating a lack of an object.
+ * byte - is a unit of data that is eight binary digits long.a byte is the unit most computers use to represent a character such as a letter, number or typographic symbol.
+ * 8 primitive data types in java:
+ * type | bytes| description
+ * 1. byte | 1 | very short integers from -128 to 127
+ * 2. short | 2 | short integers from -32,768 to 32,767
+ * 3. int | 4 | integers from -2,147,483,648 to 2,147,483,647
+ * 4. long | 8 | 	Long integers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+ * 5. float | 4 | 	Single-precision, floating-point numbers from -3.4E38 to 3.4E38 with up to 7 significant digits
+ * 6. double | 8 | 	Double-precision, floating-point numbers from -1.7E308 to 1.7E308 with up to 16 significant digits
+ * 7. char | 2 | a single unicode character thats stored in two bytes.
+ * 8. boolean | 1 | a true or false value.
+ *
+ * you can assign a value of lesser precision to a type of greater precision, but not the other way around.
  */
 
     }
+
 }
