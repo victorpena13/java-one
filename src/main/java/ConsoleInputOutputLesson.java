e61f02c5cdef9ba4d5dfaf35ee92274607103a5f
Victor Pena <13victor.pena@gmail.com>
2023-07-12 15:34:13 -0500

    printing data to the console

---
+++ src/main/java/ConsoleInputOutputLesson.java
@@ -0,0 +1,11 @@
+public class ConsoleInputOutputLesson {
+
+    public static void main(String[] args) {
+        String name = "victor";
+        System.out.printf("hello there, %s. Nice to see you", name);
+
+        String greeting = "Salutations";
+
+        System.out.printf("%s, %s!", greeting, name);
+    }
+}
