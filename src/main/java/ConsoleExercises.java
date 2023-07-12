145119e6ca1c715a62970d620eb0eaff09b2a6e3
Victor Pena <13victor.pena@gmail.com>
2023-07-12 17:30:32 -0500

    calculated area and perimiter from user input

--- src/main/java/ConsoleExercises.java
+++ src/main/java/ConsoleExercises.java
@@ -14,18 +14,24 @@ public class ConsoleExercises {
         System.out.print("enter 3 words: ");
         scanner.nextLine();
         String userString = scanner.nextLine();
-
-
-//        String userWordOne = scanner.next();
-//        String userWordTwo = scanner.next();
-//        String userWordThree = scanner.next(); // will only capture first three words and ignore all others. you must enter three words to continue or else stuck in loop.
-//        System.out.printf("you entered %s %s %s", userWordOne, userWordTwo, userWordThree);
-        String[] splited = userString.split("//s+");
-        for(String word :splited) {
+        /*
+       String userWordOne = scanner.next();
+       String userWordTwo = scanner.next();
+       String userWordThree = scanner.next(); // will only capture first three words and ignore all others. you must enter three words to continue or else stuck in loop.
+       System.out.printf("you entered %s %s %s", userWordOne, userWordTwo, userWordThree);
+        */
+        String[] split = userString.split("//s+");
+        for(String word :split) {
             System.out.println(word);
         }
 
-
+        System.out.println("enter length & width: ");
+        String userLength = scanner.nextLine();
+        String userWidth = scanner.nextLine();
+        int userArea = Integer.parseInt(userLength) * Integer.parseInt(userWidth);
+        int userPerimeter = Integer.parseInt(userLength) + Integer.parseInt(userLength) + Integer.parseInt(userWidth) + Integer.parseInt(userWidth);
+        System.out.println("area: " + userArea);
+        System.out.println("perimeter: " + userPerimeter);
 
     }
 }
