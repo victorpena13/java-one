4144bceabc75a58a68aa202e33fc3a8eb79b73e2
Victor Pena <13victor.pena@gmail.com>
2023-07-22 16:07:25 -0500

    ControlFlowExercises.java created an int i with a value of 5 a while loop that runs so long as i is less than or equal to 15

---
+++ src/main/java/ControlFlowExercises.java
@@ -0,0 +1,11 @@
+public class ControlFlowExercises {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        while (a <= 15) {
+            System.out.println(a);
+            a++;
+        }
+    }
+}
