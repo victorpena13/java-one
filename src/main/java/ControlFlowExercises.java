594b0c09c7a110e2aa3c4661efa630541b30d8e3
Victor Pena <13victor.pena@gmail.com>
2023-07-22 16:13:29 -0500

    do-while loop to increment from 0 to 100 in multiples of 2

--- src/main/java/ControlFlowExercises.java
+++ src/main/java/ControlFlowExercises.java
@@ -4,8 +4,15 @@ public class ControlFlowExercises {
 
         int a = 5;
         while (a <= 15) {
-            System.out.println(a);
+            System.out.print(a + " ");
             a++;
         }
+
+        int b = 0;
+
+        do {
+            System.out.println(b);
+            b+=2;
+        } while (b <= 100);
     }
 }
