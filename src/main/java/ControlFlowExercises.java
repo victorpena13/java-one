740acd16a128a20077b8126ba6a47162845e605f
Victor Pena <13victor.pena@gmail.com>
2023-07-22 16:38:40 -0500

    for loops and do while loops

--- src/main/java/ControlFlowExercises.java
+++ src/main/java/ControlFlowExercises.java
@@ -14,5 +14,25 @@ public class ControlFlowExercises {
             System.out.println(b);
             b+=2;
         } while (b <= 100);
+
+        int c = 100;
+
+        do{
+            System.out.println(c);
+            c-=5;
+        } while(c>=-10);
+
+
+        // must use long to perform following loop:
+        long d = 2;
+        do {
+            System.out.println(d);
+            d*=d;
+        } while(d <= 1000000);
+
+        for (int e = 100; e >= -10; e-=5) {
+            System.out.println(e);
+        }
+
     }
 }
