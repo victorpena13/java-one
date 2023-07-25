800e0553b38e7d4e312658e8fcae422aa29df95b
Victor Pena <13victor.pena@gmail.com>
2023-07-25 09:38:53 -0500

    switch statement for bob

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Bob {
 
     public static void main(String[] args) {
+//        Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("talk to bob: ");
+        String userInput = scanner.nextLine();
+
+        switch(userInput) {
+            case "?":
+                System.out.println("Sure!");
+                break;
+        }
+
+
+
 
     }
 }
