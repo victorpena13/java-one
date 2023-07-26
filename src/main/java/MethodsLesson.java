97ab009de0135e7dea1ccdf02cace8cf5b298fa6
Victor Pena <13victor.pena@gmail.com>
2023-07-26 17:42:36 -0500

    examples of methods

--- src/main/java/MethodsLesson.java
+++ src/main/java/MethodsLesson.java
@@ -1,7 +1,26 @@
+import java.util.Locale;
+
 public class MethodsLesson {
+    //public = visibility modifier
+    //static = the presence of this keyword defines that the method belongs to the class, as opposed to an instance of it.
+    //string = the return type for method
+    // all methods must define the type of value they will return.
+    // if method does not return anything we can use void.
 
     public static String sayHello(String name) {
         return String.format("Hello, %s!", name);
+    }
+
+    public static void sayHello(String greeting, String name) {
+        System.out.printf("%s, %s!\n", greeting, name);
+    }
+
+    public static int returnThree() {
+        return 3;
+    }
+
+    public static String shout(String s) {
+        return s.toUpperCase() + "!!!";
     }
 
     public static void main(String[] args) {
