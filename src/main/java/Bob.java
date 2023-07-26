457636edc64c0577de71bb0f1cfa9e7fc87871d7
Victor Pena <13victor.pena@gmail.com>
2023-07-26 17:15:36 -0500

    added do-while statement to bob.java

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -4,21 +4,23 @@ public class Bob {
 
     public static void main(String[] args) {
 //        Bob is a lackadaisical teenager. In conversation, his responses are very limited.
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("talk to bob: ");
-        String userInput = scanner.nextLine();
 
-        if(userInput.endsWith("?")) {
-            System.out.println("sure");
-        } else if(userInput.endsWith("!")) {
-            System.out.println("whoa, chill out");
-        } else if(userInput.startsWith("") & userInput.endsWith("")) {
-            System.out.println("fine, be that way");
-        } else if(userInput.equalsIgnoreCase("exit")) {
-            return;
-        } else {
-            System.out.println("whatever");
-        }
+       do {
+           System.out.println("talk to bob: ");
+           Scanner scanner = new Scanner(System.in);
+           String userInput = scanner.nextLine();
+           if (userInput.endsWith("?")) {
+               System.out.println("sure");
+           } else if (userInput.endsWith("!")) {
+               System.out.println("whoa, chill out");
+           } else if (userInput.startsWith("") & userInput.endsWith("")) {
+               System.out.println("fine, be that way");
+           } else if (userInput.equalsIgnoreCase("exit")) {
+               return;
+           } else {
+               System.out.println("whatever");
+           }
+       } while (true);
 
     }
 }
