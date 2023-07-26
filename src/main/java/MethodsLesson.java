88505d48c36635702ede453ae72600ca51872d96
Victor Pena <13victor.pena@gmail.com>
2023-07-26 17:37:59 -0500

    created a class MethodsLesson and a sayHello method along with a main method

---
+++ src/main/java/MethodsLesson.java
@@ -0,0 +1,11 @@
+public class MethodsLesson {
+
+    public static String sayHello(String name) {
+        return String.format("Hello, %s!", name);
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
