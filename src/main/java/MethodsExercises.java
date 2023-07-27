dd5734ff3ed0f9a4da6b0873094060a92164b30a
Victor Pena <13victor.pena@gmail.com>
2023-07-27 16:33:59 -0500

    multiply with loop with out *

--- src/main/java/MethodsExercises.java
+++ src/main/java/MethodsExercises.java
@@ -1,5 +1,4 @@
 public class MethodsExercises {
-
     public static int addition(int a, int b) {
         return a + b;
     }
@@ -12,11 +11,23 @@ public class MethodsExercises {
         return a * b;
     }
 
-    public static void division(int a, int b) {
-        if(a == 0 | b == 0) {
-           System.out.println("undefined");
+    public static int multiplyWithLoop(int a, int b) {
+        int result = 0;
+        for(int r = 1; r <= b; r++) {
+            result += a;
         }
-        System.out.println(a/b);
+        return result;
+    }
+
+    public static void division(int a, int b) {
+        if(a == 0) {
+           System.out.println("undefined");
+        } else if(b == 0) {
+            System.out.println("undefined");
+        } else {
+            System.out.println(a/b);
+
+        }
     }
 
     public static int modulus(int a, int b) {
@@ -30,6 +41,9 @@ public class MethodsExercises {
         division(1,2);
         division(1,0);
         System.out.println(modulus(2,1));
+        System.out.println("test: ");
+        System.out.println(multiplyWithLoop(2,5));
+        System.out.println(multiplyWithLoop(3,5));
 
     }
 }
