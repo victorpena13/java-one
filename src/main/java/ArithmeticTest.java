4e2d6cc82fa8709f745ff5fc3c7e0f18036a42b3
Victor Pena <13victor.pena@gmail.com>
2023-07-29 16:58:47 -0500

    arithmetic test

---
+++ src/main/java/ArithmeticTest.java
@@ -0,0 +1,8 @@
+public class ArithmeticTest {
+
+    public static void main(String[] args) {
+        System.out.println(Arithmetic.pi);
+        System.out.println(Arithmetic.add(5,5));
+        System.out.println(Arithmetic.multiply(5,5));
+    }
+}
