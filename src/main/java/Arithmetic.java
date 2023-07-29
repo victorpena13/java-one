943ce7ba63a8acaca098012c62ce64d97168217b
Victor Pena <13victor.pena@gmail.com>
2023-07-29 16:54:35 -0500

    created a static class called arithmetic

---
+++ src/main/java/Arithmetic.java
@@ -0,0 +1,16 @@
+// the artihmetic class is only meant to be referenced statically. meaning we never create instances of it.
+public class Arithmetic {
+
+    // static property:
+    public static double pi = 3.13159;
+
+    // static methods:
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+    public static int multiply(int x, int y) {
+        return x + y;
+    }
+
+}
