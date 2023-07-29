553a616b2bbd8efaee55319cd0cd3201993ec45b
Victor Pena <13victor.pena@gmail.com>
2023-07-29 16:39:40 -0500

    instance of person objects

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -7,10 +7,21 @@ public class Person {
     }
 
     public static void main(String[] args) {
+        // instance of the Person object:
         Person rick = new Person();
         rick.firstName = "Rick";
         rick.lastName = "Sanchez";
         System.out.println(rick.sayHello());
+
+        // instance of the person object:
+        Person ada = new Person();
+        ada.firstName = "Ada";
+        ada.lastName = "Lovelace";
+
+        // instance of the person object:
+        Person grace = new Person();
+        grace.firstName = "Grace";
+        grace.lastName = "Hopper";
 
     }
 }
