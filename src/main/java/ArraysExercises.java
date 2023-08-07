1b0b2df266b85d636bbfbda34517f480315cb6dd
Victor Pena <13victor.pena@gmail.com>
2023-08-07 00:12:25 -0500

    Arrays.toString() example

---
+++ src/main/java/ArraysExercises.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1,2,3,4,5};
+        System.out.println(Arrays.toString(numbers));
+        // has to use tostring because arrays are a reference.
+
+
+
+    }
+}
