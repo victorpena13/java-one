b08cfd8fbdd58271a81b0167776edadeba67ad59
Victor Pena <13victor.pena@gmail.com>
2023-08-06 23:28:46 -0500

    added count feature

--- src/main/java/shapes/CircleApp.java
+++ src/main/java/shapes/CircleApp.java
@@ -5,27 +5,19 @@ import util.Input;
 public class CircleApp {
 
     public static void main(String[] args) {
-
         Input input = new Input();
-
-        boolean yesNo;
-        do {
-            System.out.print("enter the radius of your circle: ");
-            double userRadius = input.getDouble();
-            Circle circleOne = new Circle(userRadius);
-            System.out.println("area of circle: " + circleOne.getArea());
-            System.out.println("circumference of circle: " + circleOne.getCircumference());
-            input.getString();
-            yesNo = input.yesNo();
-        } while(yesNo);
-
-
-
-
-
-
-
-
-
+        boolean yesNo = true;
+        int count = 0;
+            do {
+                count++;
+                    System.out.print("enter the radius of your circle: ");
+                    double userRadius = input.getDouble();
+                    Circle circleOne = new Circle(userRadius);
+                    System.out.println("area of circle: " + circleOne.getArea());
+                    System.out.println("circumference of circle: " + circleOne.getCircumference());
+                    System.out.println("circle count: " + count);
+                    input.getString();
+                    yesNo = input.yesNo();
+            } while(yesNo);
     }
 }
