9ae48d77fc1f675ad36271cbe24e1a5809a2c806
Victor Pena <13victor.pena@gmail.com>
2023-10-29 06:09:23 -0500

    tested saving a char value to a string variable

--- src/main/java/SyntaxTypesVariables.java
+++ src/main/java/SyntaxTypesVariables.java
@@ -4,5 +4,10 @@ public class SyntaxTypesVariables {
         int myFavoriteNumber = 13;
         System.out.println(myFavoriteNumber);
 
+        String myString = "this is a string example";
+        System.out.println(myString);
+
+        // myString = 'a'; error because i provided char(single character) not string.
+
     }
 }
