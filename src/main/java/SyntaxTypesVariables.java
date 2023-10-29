67f0c8f6998ddc8ba00c4a9b98381cba8817be15
Victor Pena <13victor.pena@gmail.com>
2023-10-29 06:12:28 -0500

    cannot run program until variable has been intialized

--- src/main/java/SyntaxTypesVariables.java
+++ src/main/java/SyntaxTypesVariables.java
@@ -10,5 +10,8 @@ public class SyntaxTypesVariables {
         // myString = 'a'; error because i provided char(single character) not string.
         // myString = 3.13149; error because i provided a double and needs a string value.
 
+        long myNumber;
+//        System.out.println(myNumber); error:variable not intialized.
+
     }
 }
