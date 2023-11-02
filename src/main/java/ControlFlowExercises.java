7335a11685fe17f2b70098f794aa8cd9123c1d08
Victor Pena <13victor.pena@gmail.com>
2023-11-02 07:49:19 -0500

    do-while and while loops

--- src/main/java/ControlFlowExercises.java
+++ src/main/java/ControlFlowExercises.java
@@ -3,5 +3,24 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
+        int a = 5;
+        while(a <= 15) {
+            System.out.print(a++ + " ");
+        }
+
+        int b = 0;
+        do {
+            System.out.println(b+=2);
+        } while(b < 100);
+
+        int c = 100;
+        do {
+            System.out.println(c-=5);
+        } while(c > -10);
+
+
+
+
+
     }
 }
