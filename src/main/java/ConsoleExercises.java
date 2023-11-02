885880edd29df54e9da1298eb852a84139fbeea7
Victor Pena <13victor.pena@gmail.com>
2023-11-01 21:05:53 -0500

    2 different versions for formatting how many places passed the decimal point should be displayed

--- src/main/java/ConsoleExercises.java
+++ src/main/java/ConsoleExercises.java
@@ -3,35 +3,13 @@ import java.util.Scanner;
 public class ConsoleExercises {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double pi = 3.14159;
 
+        double pi = 3.14159;
+        System.out.printf("the value of pi is approximately %2.4s%n", pi);
         System.out.printf("the value of pi is approximately %.2f", pi);
 
-        System.out.print("\nenter a integer: ");
-        int userInt = scanner.nextInt(); //if input is not an integer will throw an exception
-
-        System.out.print("enter 3 words: ");
-        scanner.nextLine();
-        String userString = scanner.nextLine();
-        /*
-       String userWordOne = scanner.next();
-       String userWordTwo = scanner.next();
-       String userWordThree = scanner.next(); // will only capture first three words and ignore all others. you must enter three words to continue or else stuck in loop.
-       System.out.printf("you entered %s %s %s", userWordOne, userWordTwo, userWordThree);
-        */
-        String[] split = userString.split("//s+");
-        for(String word :split) {
-            System.out.println(word);
-        }
-
-        System.out.println("enter length & width: ");
-        String userLength = scanner.nextLine();
-        String userWidth = scanner.nextLine();
-        int userArea = Integer.parseInt(userLength) * Integer.parseInt(userWidth);
-        int userPerimeter = Integer.parseInt(userLength) + Integer.parseInt(userLength) + Integer.parseInt(userWidth) + Integer.parseInt(userWidth);
-        System.out.println("area: " + userArea);
-        System.out.println("perimeter: " + userPerimeter);
 
     }
+
+
 }
