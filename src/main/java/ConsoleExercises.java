d8baf6e531f7d3162601d3ac8394e1e8fd483083
Victor Pena <13victor.pena@gmail.com>
2023-11-01 21:14:10 -0500

    prompted a user to enter an integer and stored the value in an int variable using the nextInt method from the scanner class

--- src/main/java/ConsoleExercises.java
+++ src/main/java/ConsoleExercises.java
@@ -1,12 +1,24 @@
+import util.Input;
+
 import java.util.Scanner;
 
 public class ConsoleExercises {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         double pi = 3.14159;
-        System.out.printf("the value of pi is approximately %2.4s%n", pi);
-        System.out.printf("the value of pi is approximately %.2f", pi);
+        System.out.printf("the value of pi is approximately %2.4s\n", pi);
+        System.out.printf("the value of pi is approximately %.2f\n", pi);
+
+        System.out.print("enter a integer and i will store that value: ");
+        int userInt = scanner.nextInt();
+
+        System.out.println("you entered: " + userInt);
+
+
+
+
 
 
     }
