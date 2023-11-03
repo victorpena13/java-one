142675fc6d544c6996c0d450333eb741741bc905
Victor Pena <13victor.pena@gmail.com>
2023-11-03 08:50:55 -0500

    four separte methods: addition, subtraction, multiplication, division - all return an int value

--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Methods {
+public class Methods {
+
+    public static int addition(int x, int y) {
+        return x+y;
+    }
+
+    public static int subtraction(int x, int y) {
+        return x-y;
+    }
+
+    public static int multiplication(int x, int y) {
+        return x*y;
+    }
+
+    public static int division(int x, int y) {
+        return x/y;
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
 }
