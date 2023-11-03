94d7fc03f17972e5a86c9a4c279aba0da95af825
Victor Pena <13victor.pena@gmail.com>
2023-11-03 08:56:45 -0500

    tested methods and verified output

--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -16,9 +16,16 @@ public class Methods {
         return x/y;
     }
 
+    public static int modulus(int x, int y) {
+        return x%y;
+    }
 
 
     public static void main(String[] args) {
-
+        System.out.println(addition(2,2)); //4
+        System.out.println(subtraction(2,2)); //0
+        System.out.println(multiplication(2,2)); //4
+        System.out.println(division(2,2)); //1
+        System.out.println(modulus(2,2)); //0
     }
 }
