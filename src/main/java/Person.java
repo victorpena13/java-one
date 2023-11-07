e6d448427067d63f81a6f8d9e755d580a1792fb4
Victor Pena <13victor.pena@gmail.com>
2023-11-07 10:18:05 -0600

    updated Person class with a constructor that sets private variable name

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,23 +1,20 @@
-// defined a class named Person. with 2 instance variables (firstname & lastname). the class also defines an instance method, sayHello.
 public class Person {
-    //static instance:
-    public static long wordPopulation = 7_500_000_000L;
-    public String firstName;
-    public String lastName;
 
-    public String sayHello() {
-        return String.format("Hello from %s %s!", firstName, lastName);
+    private String name;
+
+    public Person(String name) {
 
     }
-    public static void main(String[] args) {
-        //inside main method, created an instance of the Person class'
 
-        Person rick = new Person();
-        rick.firstName = "Rick";
-        rick.lastName = "Sanchez";
-        System.out.println(rick.sayHello());
-
-
-
+    public String getName() {
+        return name;
     }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello, " + name);
+    }
+
 }
